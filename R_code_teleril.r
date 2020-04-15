2448b13348d63afa0714626b2dab8ebe25916163
ducciorocchini <ducciorocchini@gmail.com>
2020-04-15 12:00:03 +0100

    Update R_code_teleril.r

--- R_code_teleril.r
+++ R_code_teleril.r
@@ -68,7 +68,7 @@ plot(p224r63_2011$B1_sre, col=clb)
 clg <- colorRampPalette(c('dark green','green','light green'))(100) # 
 plot(p224r63_2011$B2_sre, col=clg)
 
-# green
+# red
 clr <- colorRampPalette(c('dark red','red','pink'))(100) # 
 plot(p224r63_2011$B3_sre, col=clr)
 
@@ -114,14 +114,125 @@ plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
 # Exercise: nir nella componente B(Blue)
 plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="Lin")
 
+###--- day 2
 
+# install.apckages("raster")
+library(raster)
 
+setwd("~/lab/") # linux
+# setwd("C:/lab/") # windows
+# setwd("/Users/nome/Desktop/lab") # mac
 
+load("teleril.RData")
 
+# list
+ls()
 
+p224r63_1988 <- brick("p224r63_1988_masked.grd")
 
+plot(p224r63_1988)
 
+# multiframe
+par(mfrow=c(2,2))
 
+# blue
+clb <- colorRampPalette(c('dark blue','blue','light blue'))(100) # 
+plot(p224r63_1988$B1_sre, col=clb)
+
+# green
+clg <- colorRampPalette(c('dark green','green','light green'))(100) # 
+plot(p224r63_1988$B2_sre, col=clg)
+
+# red
+clr <- colorRampPalette(c('dark red','red','pink'))(100) # 
+plot(p224r63_1988$B3_sre, col=clr)
+
+# nir
+clnir <- colorRampPalette(c('red','orange','yellow'))(100) # 
+plot(p224r63_1988$B4_sre, col=clnir)
+
+dev.off()
+
+# B1: blue - 1
+# B2: green - 2
+# B3: red - 3
+# B4: near infrared (nir) - 4 
+
+plotRGB(p224r63_1988, r=3, g=2, b=1, stretch="Lin")
+
+# Exercise: plot the image using the nir on the "r" component in the RGB space
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="Lin")
+
+# plot delle due immagini 1988 e 2011
+par(mfrow=c(2,1))
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="Lin", main="1988")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin", main="2011")
+dev.off()
+
+# spectral indices
+# dvi1988 = nir1988-red1988
+
+dvi1988 <- p224r63_1988$B4_sre - p224r63_1988$B3_sre
+plot(dvi1988)
+
+# Exercise: calculate dvi for 2011
+dvi2011 <- p224r63_2011$B4_sre - p224r63_2011$B3_sre
+plot(dvi2011)
+
+cldvi <- colorRampPalette(c('light blue','light green','green'))(100) # 
+plot(dvi2011, col=cldvi)
+
+# multitemporal analysis
+difdvi <- dvi2011 - dvi1988
+plot(difdvi)
+cldifdvi <- colorRampPalette(c('red','white','blue'))(100) # 
+plot(difdvi, col=cldifdvi)
+
+# visualize the output
+# multiframe 1988rgb, 2011rgb, difdiv
+par(mfrow=c(3,1))
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+plot(difdvi, col=cldifdvi)
+dev.off()
+
+# Changing the grain (resolution)
+p224r63_2011lr <- aggregate(p224r63_2011, fact=10)
+p224r63_2011
+p224r63_2011lr
+
+par(mfrow=c(2,1))
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011lr, r=4, g=3, b=2, stretch="Lin")
+dev.off()
+
+# lower resolution
+p224r63_2011lr50 <- aggregate(p224r63_2011, fact=50)
+p224r63_2011lr50
+# original 30m -> resampled 1500m
+
+par(mfrow=c(3,1))
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011lr, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011lr50, r=4, g=3, b=2, stretch="Lin")
+
+# dvi2011 low resolution
+dvi2011lr50 <- p224r63_2011lr50$B4_sre - p224r63_2011lr50$B3_sre
+plot(dvi2011lr50)
+
+# dvi1988 low resolution
+p224r63_1988lr50 <- aggregate(p224r63_1988, fact=50)
+dvi1988lr50 <- p224r63_1988lr50$B4_sre - p224r63_1988lr50$B3_sre
+
+# difdvi low resolution
+difdvilr50 <- dvi2011lr50 - dvi1988lr50
+cldifdvi <- colorRampPalette(c('red','white','blue'))(100) # 
+plot(difdvilr50, col=cldifdvi)
+
+# multiframe
+par(mfrow=c(2,1))
+plot(difdvi, col=cldifdvi)
+plot(difdvilr50, col=cldifdvi)
 
 
 
