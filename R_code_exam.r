3f8c8dc45281028cd6faa97eb30ce27b548f0f9e
ducciorocchini <ducciorocchini@gmail.com>
2020-06-08 11:49:14 +0100

    Update R_code_exam.r

--- R_code_exam.r
+++ R_code_exam.r
@@ -12,7 +12,8 @@
 # 8. R_code_multitemp_NO2.r	
 # 9. R_code_snow.r	
 # 10. R_code_patches.r	
-# 11. R_code_crop.r -  exam simulation
+# 11. R_code_crop.r -  exam simulation. 
+# 12. Species Distribution Modelling
 
 #################################################################
 #################################################################
@@ -1093,10 +1094,70 @@ plot(snow.multitemp.italy, col=clb, zlim=c(20,200))
 # boxplot
 boxplot(snow.multitemp.italy, horizontal=T,outline=F)
 
+#################################################################
+#################################################################
+#################################################################
+
+### 12. Species Distribution Modelling
+
+# install.packages("sdm")
+# install.packages("rgdal")
+library(sdm)
+library(raster)
+library(rgdal)
+
+# species
+file <- system.file("external/species.shp", package="sdm") 
+species <- shapefile(file)
+
+species
+species$Occurrence
+plot(species)
+
+plot(species[species$Occurrence == 1,],col='blue',pch=16)
+
+points(species[species$Occurrence == 0,],col='red',pch=16)
+
+# environmental variables
+path <- system.file("external", package="sdm") 
+
+lst <- list.files(path=path,pattern='asc$',full.names = T) #
+lst
+
+preds <- stack(lst)
+
+cl <- colorRampPalette(c('blue','orange','red','yellow')) (100)
+plot(preds, col=cl)
+
+plot(preds$elevation, col=cl)
+points(species[species$Occurrence == 1,], pch=16)
+
+plot(preds$temperature, col=cl)
+points(species[species$Occurrence == 1,], pch=16)
+
+plot(preds$precipitation, col=cl)
+points(species[species$Occurrence == 1,], pch=16)
+
+plot(preds$vegetation, col=cl)
+points(species[species$Occurrence == 1,], pch=16)
+
+# model
+
+d <- sdmData(train=species, predictors=preds)
+d
+
+m1 <- sdm(Occurrence ~ elevation + precipitation + temperature + vegetation, data=d, methods='glm') 
+p1 <- predict(m1, newdata=preds)
+
+plot(p1, col=cl)
+points(species[species$Occurrence == 1,], pch=16)
 
 
+####################################################
 
+# EXAM PROJECT
 
+# Here put your code
 
 
 
